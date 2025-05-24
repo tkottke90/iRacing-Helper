18a95809b4827711675f5e498e99581f4f3b080a
Thomas Kottke <t.kottke90@gmail.com>
2025-05-24 11:36:31 -0500

    refactor: update controller index to use versioning

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -2,30 +2,20 @@
 // individual controller files in this directory, import them here
 // and then add them to the router setup
 
-import { Application, Router } from 'express';
+import { Application } from 'express';
 import { ServerStatusController } from './server-status';
 import { iRacingController } from './iracing';
 import { MCPController } from './mcp.controller';
 import { CarController } from './car.controller';
-import { attachController, Controller } from 'express-ts-decorators';
+import { attachVersionMap, createVersionMap } from 'express-ts-decorators';
 
-const Routers: Record<string, Array<Controller>> = {
-  '/v1': [
-    new iRacingController(),
-    new ServerStatusController(),
-    new MCPController(),
-    new CarController()
-  ]
-};
+const VersionMap = createVersionMap([
+  new iRacingController(),
+  new ServerStatusController(),
+  new MCPController(),
+  new CarController()
+]);
 
 export default function (app: Application) {
-  for (const path in Routers) {
-    const router = Router({ mergeParams: true });
-
-    for (const Controller of Routers[path]) {
-      attachController(router, Controller);
-    }
-
-    app.use(path, router);
-  }
+  attachVersionMap(app, VersionMap);
 }
