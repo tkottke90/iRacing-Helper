de58e5c05d0804842cbafd5eafe83081026fb6cc
Thomas Kottke <t.kottke90@gmail.com>
2025-05-24 11:01:28 -0500

    refactor: update generics for http decorators

--- express-ts-decorators/src/decorators/patch.decorator.ts
+++ express-ts-decorators/src/decorators/patch.decorator.ts
@@ -8,12 +8,18 @@ import { Errors } from '../utils/error.utils';
  * @param middleware Middleware functions to apply to the request
  */
 export function Patch<
-  Request = express.Request,
-  Response = express.Response,
-  Next = express.NextFunction
+  Body extends express.Request['body'] = any,
+  Query extends express.Request['query'] = any,
+  Params extends express.Request['params'] = any,
+  Locals extends express.Response['locals'] = any,
+  ResBody = any
 >(path: string, middleware: express.RequestHandler[] = []) {
   return function actualDecorator(
-    _originalMethod: (...args: any[]) => any,
+    _originalMethod: (
+      req: express.Request<Params, ResBody, Body, Query>,
+      res: express.Response<ResBody, Locals>,
+      next?: express.NextFunction
+    ) => void,
     context: ClassMethodDecoratorContext
   ) {
     // Register the route to the controller during initialization
